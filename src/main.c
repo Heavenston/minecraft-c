ebf231de52c292bc1016aacfae97edeb92abcb0c
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-05-01 14:56:36 +0200

    feat: Vertex buffer

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include "safe_cast.h"
 #include "window/window.h"
 #include "cpu_rasterizer/cpu_rasterizer.h"
+#include "linalg/matrix.h"
+#include "linalg/transformations.h"
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 struct shader_data {
-    uint32_t unused;
+    mcc_vec3f *positions;
+    mcc_vec4f *colors;
+    size_t vertex_count;
+    mcc_mat4f mvp;
 };
 
 /**
@@ -15,24 +21,13 @@ struct shader_data {
 const uint32_t SHADERS_VARYING_COUNT = 1;
 
 void my_vertex_shader_fn(struct mcc_cpurast_vertex_shader_input *input) {
-    switch (input->in_vertex_idx) {
-    case 0:
-        input->out_position = (mcc_vec4f){{ -1., -1., 0., 1. }};
-        input->r_out_varyings[0].vec4f = (mcc_vec4f){{ 0., 0., 0., 0. }};
-        break;
-    case 1:
-        input->out_position = (mcc_vec4f){{ -1.,  1., 0., 1. }};
-        input->r_out_varyings[0].vec4f = (mcc_vec4f){{ 0., 1., 0., 0. }};
-        break;
-    case 2:
-        input->out_position = (mcc_vec4f){{  1., -1., 0., 1. }};
-        input->r_out_varyings[0].vec4f = (mcc_vec4f){{ 1., 0., 0., 0. }};
-        break;
-    default:
-        fprintf(stderr, "Tried to run vertex shader with out of bound vertex index\n");
-        input->out_position = (mcc_vec4f){{}};
-        break;
-    }
+    struct shader_data *data = input->o_in_data;
+
+    mcc_vec3f pos3 = data->positions[input->in_vertex_idx];
+    mcc_vec4f pos4 = (mcc_vec4f){{ pos3.x, pos3.y, pos3.z, 1.f }};
+    input->out_position = mcc_mat4f_mul_vec4f(data->mvp, pos4);
+    // input->out_position = (mcc_vec4f){{ pos3.x, pos3.y, pos3.z, 1.f }};
+    input->r_out_varyings[0].vec4f = data->colors[input->in_vertex_idx];
 }
 
 void my_fragment_shader_fn(struct mcc_cpurast_fragment_shader_input *input) {
@@ -49,6 +44,48 @@ int main() {
     });
 
     mcc_window_open(window);
+
+    mcc_vec3f vertices[] = {
+        {{-1.f,-1.f, 0.f}},
+        {{ 1.f,-1.f, 0.f}},
+        {{-1.f, 1.f, 0.f}},
+    };
+    mcc_vec4f colors[] = {
+        {{ 1.f, 0.f, 0.f, 1.f }},
+        {{ 0.f, 1.f, 0.f, 1.f }},
+        {{ 0.f, 0.f, 1.f, 1.f }},
+    };
+
+    struct shader_data shader_data = {
+        .positions = vertices,
+        .colors = colors,
+        .vertex_count = sizeof(vertices) / sizeof(*vertices),
+        .mvp = mcc_mat4f_scale_xyz(0.5f, 0.5f, 0.5f),
+    };
+
+    struct mcc_vertex_shader vertex_shader = {
+        .r_fn = my_vertex_shader_fn,
+        .varying_count = SHADERS_VARYING_COUNT,
+    };
+    struct mcc_fragment_shader fragment_shader = {
+        .r_fn = my_fragment_shader_fn,
+        .varying_count = SHADERS_VARYING_COUNT,
+    };
+
+    struct mcc_cpurast_render_config render_config = {
+        .r_attachment = NULL, // Set before each render
+
+        .o_vertex_shader_data = &shader_data,
+        .r_vertex_shader = &vertex_shader,
+
+        .o_fragment_shader_data = &shader_data,
+        .r_fragment_shader = &fragment_shader,
+
+        .culling_mode = MCC_CPURAST_CULLING_MODE_NONE,
+        .o_depth_comparison_fn = NULL,
+
+        .vertex_count = shader_data.vertex_count,
+    };
 
     for (bool close = false; !close;) {
         union mcc_window_event event = mcc_window_wait_next_event(window);
@@ -73,45 +110,16 @@ int main() {
             size_t height = safe_to_size_t(geometry.height);
             size_t width = safe_to_size_t(geometry.width);
 
-            struct shader_data shader_data = {};
-
             size_t bytes = height * width * 4;
             uint8_t *image_data = calloc(1, bytes);
-            struct mcc_cpurast_rendering_color_attachment color_attachment = {
-                .r_data = image_data,
-            };
 
             struct mcc_cpurast_rendering_attachment attachment = {
-                .o_depth = NULL,
-                .o_color = &color_attachment,
-                .width = safe_to_u32(width),
-                .height = safe_to_u32(height),
+                .o_depth = NULL, // No depth buffer
+                .o_color = &(struct mcc_cpurast_rendering_color_attachment) { .r_data = image_data, },
+                .width = width,
+                .height = height,
             };
-
-            struct mcc_vertex_shader vertex_shader = {
-                .r_fn = my_vertex_shader_fn,
-                .varying_count = SHADERS_VARYING_COUNT,
-            };
-            struct mcc_fragment_shader fragment_shader = {
-                .r_fn = my_fragment_shader_fn,
-                .varying_count = SHADERS_VARYING_COUNT,
-            };
-
-            struct mcc_cpurast_render_config render_config = {
-                .r_attachment = &attachment,
-
-                .o_vertex_shader_data = &shader_data,
-                .r_vertex_shader = &vertex_shader,
-
-                .o_fragment_shader_data = &shader_data,
-                .r_fragment_shader = &fragment_shader,
-
-                .culling_mode = MCC_CPURAST_CULLING_MODE_NONE,
-                .o_depth_comparison_fn = NULL,
-
-                .vertex_count = 3,
-            };
-
+            render_config.r_attachment = &attachment;
             mcc_cpurast_render(&render_config);
 
             mcc_window_put_image(window, image_data, geometry.width, geometry.height);
