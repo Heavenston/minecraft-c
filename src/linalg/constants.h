6a156ce1ea1b9c5c1e8efcd0a604491cc2dc601e
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-05-01 14:25:17 +0200

    feat: Constants

---
+++ src/linalg/constants.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define MCC_E          2.7182818284590452354    /* e */
+#define MCC_LOG2E      1.4426950408889634074    /* log_2 e */
+#define MCC_LOG10E     0.43429448190325182765   /* log_10 e */
+#define MCC_LN2        0.69314718055994530942   /* log_e 2 */
+#define MCC_LN10       2.30258509299404568402   /* log_e 10 */
+#define MCC_PI         3.14159265358979323846   /* pi */
+#define MCC_PI_2       1.57079632679489661923   /* pi/2 */
+#define MCC_PI_4       0.78539816339744830962   /* pi/4 */
+#define MCC_1_PI       0.31830988618379067154   /* 1/pi */
+#define MCC_2_PI       0.63661977236758134308   /* 2/pi */
+#define MCC_2_SQRTPI   1.12837916709551257390   /* 2/sqrt(pi) */
+#define MCC_SQRT2      1.41421356237309504880   /* sqrt(2) */
+#define MCC_SQRT1_2    0.70710678118654752440   /* 1/sqrt(2) */
+
+#define MCC_Ef         2.7182818284590452354f   /* e */
+#define MCC_LOG2Ef     1.4426950408889634074f   /* log_2 e */
+#define MCC_LOG10Ef    0.43429448190325182765f  /* log_10 e */
+#define MCC_LN2f       0.69314718055994530942f  /* log_e 2 */
+#define MCC_LN10f      2.30258509299404568402f  /* log_e 10 */
+#define MCC_PIf        3.14159265358979323846f  /* pi */
+#define MCC_PI_2f      1.57079632679489661923f  /* pi/2 */
+#define MCC_PI_4f      0.78539816339744830962f  /* pi/4 */
+#define MCC_1_PIf      0.31830988618379067154f  /* 1/pi */
+#define MCC_2_PIf      0.63661977236758134308f  /* 2/pi */
+#define MCC_2_SQRTPIf  1.12837916709551257390f  /* 2/sqrt(pi) */
+#define MCC_SQRT2f     1.41421356237309504880f  /* sqrt(2) */
+#define MCC_SQRT1_2f   0.70710678118654752440f  /* 1/sqrt(2) */
