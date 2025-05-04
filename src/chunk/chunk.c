7ff5f5df7e8031d2c73e6a59061f382ff3045e52
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-05-04 16:52:02 +0200

    feat: More dirt

--- src/chunk/chunk.c
+++ src/chunk/chunk.c
@@ -1,38 +1,20 @@
 #include "chunk.h"
 
 static enum mcc_block_type mcc_chunk_get_block(mcc_world_seed_t, size_t x, size_t y, size_t z) {
-    const size_t grass_height = 20;
-
-    if (x == 8 && z == 8 && y >= 3 && y <= 8)
+    if (x == 8 && z == 8 && y >= 4 && y <= 9)
         return MCC_BLOCK_TYPE_LOG;
 
-    if (x >= 6 && z >= 6 && x <= 10 && z <= 10 && y >= 7 && y <= 9)
+    if (x >= 6 && z >= 6 && x <= 10 && z <= 10 && y >= 8 && y <= 10)
         return MCC_BLOCK_TYPE_LEAVES;
  
     if (y == 0)
         return MCC_BLOCK_TYPE_STONE;
-    else if (y == 1)
+    else if (y == 1 || y == 2)
         return MCC_BLOCK_TYPE_DIRT;
-    else if (y == 2)
+    else if (y == 3)
         return MCC_BLOCK_TYPE_GRASS;
     else
         return MCC_BLOCK_TYPE_AIR;
-
-    if (z > 4)
-        return MCC_BLOCK_TYPE_AIR;
-    if (x > 4)
-        return MCC_BLOCK_TYPE_AIR;
-
-    if (y < grass_height - 10) {
-        return MCC_BLOCK_TYPE_STONE;
-    }
-    if (y < grass_height) {
-        return MCC_BLOCK_TYPE_DIRT;
-    }
-    if (y == grass_height) {
-        return MCC_BLOCK_TYPE_GRASS;
-    }
-    return MCC_BLOCK_TYPE_AIR;
 }
 
 void mcc_chunk_generate(mcc_world_seed_t seed, struct mcc_chunk_data *chunk) {
